02eb4c518c189740523977f875f7917aae2d260f
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-08-02 22:35:26 +0100

    Will be finished tomorrow.

--- simple-counter/src/components/counter.jsx
+++ simple-counter/src/components/counter.jsx
@@ -2,32 +2,30 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
    state = {
-       count: 1,
-       tags: ['tag1','tag2','tag3']
+       count: this.props.counter.value
+  
    };
-   
+ 
    getBadgeClasses() {
     let classes = " badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.count === 0 ? "warning" : "primary";
     return classes;
 };
+
 
     render() { 
 
     return (
     <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-            {this.state.tags.map(tag => <li>{ tag }</li>)}
-        </ul>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+        <button className="btn btn-danger btn-sm m-2" onClick={() =>this.props.onDelete(this.props.counter.id)}>Delete</button>
+       
     </div>);
     }
 
-
-
     formatCount(){
-        const {count} = this.state;
+        const {count} = this.props.counter;
         return this.state.count === 0 ? 'zero' : this.state.count;
     
     };
